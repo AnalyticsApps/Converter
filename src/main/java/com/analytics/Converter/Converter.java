e1d084dcb89b574f6be0193f2fbabab4d0737860
nisanth.m.s@gmail.com <root@bianalysis1.fyre.ibm.com>
2019-04-15 04:59:40 -0400

    Added test cases for Converter

--- src/main/java/com/analytics/Converter/Converter.java
+++ src/main/java/com/analytics/Converter/Converter.java
@@ -26,7 +26,7 @@ public class Converter {
 	private static final Logger LOG = Logger.getLogger(Converter.class);
 
 	public static void main(String[] args) throws Exception{
-		LOG.info(" Initiating the File Converter");
+		System.out.println(" Initiating the File Converter");
 		
 		//Get the input details
 		Scanner in = new Scanner(System.in);
@@ -37,17 +37,17 @@ public class Converter {
 		System.out.print("Path to Out file: ");
 		String outFile = in.nextLine();
 		
-		LOG.debug(String.format(" Metadata Path: %s Input File Path: %s Output File Path: %s", metadataFile, dataFile,
+		LOG.info(String.format(" Metadata Path: %s Input File Path: %s Output File Path: %s", metadataFile, dataFile,
 				outFile));
 				
 		MetadataReader reader = new MetadataReader();
 		List<Metadata> metadataList = reader.readMetadata(metadataFile);
-		LOG.debug(" Extracted Metadata Info: " + metadataList.toString());
+		LOG.info(" Extracted Metadata Info: " + metadataList.toString());
 		
 		Converter inst = new Converter();
 		inst.convert(dataFile, metadataList, outFile);
 		
-		LOG.info(" File conversion completed.");
+		System.out.print(" File conversion completed.");
 	}
 
 	/**
@@ -57,16 +57,18 @@ public class Converter {
 	 * @param metadataList
 	 * @param outFile
 	 */
-	public void convert(String dataFile, List<Metadata> metadataList, String outFile) {
+	public void convert(String dataFile, List<Metadata> metadataList, String outFile) throws IOException{
 		String line = "";
 		long index = 1;
+		long batchLoop = 2;
 		boolean isFirstBatch = true;
 		// Holds the output to write to output file
 		List<String> resultset = new ArrayList<String>();
+		int metadataTotalLength = 0;
 		
 		boolean isFirstLoop = true;
 		StringBuffer header = new StringBuffer();
-		// Adding the column details in the output file
+		// Adding the column details in the output file & check the total length of the metadata columns defined. 
 		for (Metadata metadata : metadataList) {
 			if(isFirstLoop) {
 				header.append(metadata.getColName());
@@ -74,28 +76,36 @@ public class Converter {
 			}else {
 				header.append(MetadataConstants.METADATA_COLUMN_DELIMITER + metadata.getColName());
 			}
+			metadataTotalLength +=  Integer.parseInt(metadata.getColLength());
 		}
 		resultset.add(header.toString());
 		
-		LOG.debug(" Resultset after adding header details: " + resultset.toString());
+		LOG.info(" Resultset after adding header details: " + resultset.toString());
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(dataFile))) {
 			while ((line = br.readLine()) != null) {
 				try {
-					resultset.add(transform(line, metadataList));
+					resultset.add(transform(line, metadataList, metadataTotalLength));
 				} catch (ParseException e) {
-					LOG.error(String.format(" %s in processing for line - %n: %s", e.getMessage(), (index+1), line));					
+					String err = String.format(" %s in processing the line - %d: %s", e.getMessage(), (index+1), line);
+					LOG.error(err);
+					System.out.println(err);
 				}
 				
 				// Process the data in a batch to minimize the memory footprint and helps in processing huge data file. 
 				if(index % BATCH_SIZE == 0) {
 					if(isFirstBatch) {
-						LOG.debug(String.format(" Writing the first batch of resultset to %s", outFile));
+						String msg = String.format(" Writing the first batch of resultset to %s", outFile);
+						LOG.info(msg);
+						System.out.println(msg);
 						writeToFile(outFile, false, resultset);
 					}else {
-						LOG.debug(String.format(" Writing the subsequent batch of resultset to %s", outFile));						
+						String msg = String.format(" Writing the %d batch of resultset to %s", batchLoop, outFile);
+						LOG.debug(msg);
+						System.out.println(msg);
 						//Append the processed data to the output file
 						writeToFile(outFile, true, resultset);
+						batchLoop++;
 					}
 					resultset.clear();
 				}					
@@ -118,16 +128,24 @@ public class Converter {
 	 * 
 	 * @param line
 	 * @param metadataList
+	 * @oaram metadataTotalLength
 	 * @return
 	 * @throws ParseException
 	 */
-	private String transform(String line, List<Metadata> metadataList) throws ParseException {
+	private String transform(String line, List<Metadata> metadataList, int metadataTotalLength) throws ParseException {
 		StringBuffer valueBuff = new StringBuffer();
 		int startIndex = 0;
 		int endIndex = 0;
 		int index = 0;
 		SimpleDateFormat dateformatInp = new SimpleDateFormat(INPUT_DATE_FORMAT);
 		SimpleDateFormat dateformatOut = new SimpleDateFormat(OUTPUT_DATE_FORMAT);
+		
+		// Check length of line is greater than the total length of metadata
+		if(metadataTotalLength > line.length()) {
+			// Throw ParseException mentioning length of line is less than total length of metadata
+			throw new ParseException("Error in parsing ( Length of line is less than total length of metadata columns )", 0);
+		}
+		
 		for (Metadata metadata : metadataList) {
 			if (index == 0) {
 				endIndex = Integer.parseInt(metadata.getColLength());
@@ -169,7 +187,7 @@ public class Converter {
 				dt = dateformatInp.parse(val);
 			} catch (ParseException e) {
 				// Throw ParseException mentioning error in parsing date
-				throw new ParseException(" Date parse error ", startIndex);
+				throw new ParseException("Error in parsing date ( Expected date format is yyyy-MM-dd )", startIndex);
 			}
 			val = dateformatOut.format(dt);
 		}
@@ -186,7 +204,7 @@ public class Converter {
 			boolean numeric = val.matches("-?\\d+(\\.\\d+)?");
 			if(!numeric) {
 				// Throw ParseException mentioning invalid numeric value
-				throw new ParseException(" Numeric value invalid error ", startIndex);
+				throw new ParseException("Invalid numeric data (Expected numeric value)", startIndex);
 			}
 		}
 
@@ -201,18 +219,18 @@ public class Converter {
 	 * @param isAppend
 	 * @param resultset
 	 */
-	private void writeToFile(String outFile, boolean isAppend, List<String> resultset) {
+	private void writeToFile(String outFile, boolean isAppend, List<String> resultset) throws IOException {
+		BufferedWriter writer = null;
 		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(outFile, isAppend));
+			writer = new BufferedWriter(new FileWriter(outFile, isAppend));
 			for (String line : resultset) {
 				writer.write(line + MetadataConstants.ROW_DELIMITER);
 			}
 			writer.flush();
 			writer.close();
-		} catch (IOException e) {
-			LOG.error(" IO Error in writing outputfile ", e);
-			System.exit(1);
+		} finally {
+			if(writer != null)
+				writer.close();
 		}
 	}
-
 }
--- src/test/java/com/analytics/Converter/ConverterTest.java
+++ src/test/java/com/analytics/Converter/ConverterTest.java
@@ -19,23 +19,115 @@ class ConverterTest {
 
 	@Test
 	public void convertTest1() throws Exception {
-		
-		String metadataPath = new File("src/test/resources/converter/testcase_1/metadata.txt").getAbsolutePath();
+		/**
+		 * Test Case 1 - Convert the data file based on metadata.
+		 *               Write the records to the output file.
+		 * 
+		 * Input file: src/test/resources/converter/testcase_1/input.txt
+		 * Metadata file: src/test/resources/converter/testcase_1/metadata.txt
+		 * Expected output file: src/test/resources/converter/testcase_1/expectedOutput.txt
+		 * Output file: src/test/resources/converter/testcase_1/output.txt
+		 */		
+		String testcase1Path = "src/test/resources/converter/testcase_1";
+		String metadataPath = new File(testcase1Path + "/metadata.txt").getAbsolutePath();
 		MetadataReader metadataReader = new MetadataReader();
 		List<Metadata> listMetadata = metadataReader.readMetadata(metadataPath);
 		
-		String inputFile = new File("src/test/resources/converter/testcase_1/input.txt").getAbsolutePath();
-		File outFile = new File("src/test/resources/converter/testcase_1/output.txt");
+		String inputFile = new File(testcase1Path + "/input.txt").getAbsolutePath();
+		File outFile = new File(testcase1Path + "/output.txt");
 		outFile.delete();
 		Converter inst = new Converter();
 		inst.convert(inputFile, listMetadata, outFile.getAbsolutePath());
 		
-		File expectedOutputFile = new File("src/test/resources/converter/testcase_1/expectedOutput.txt");
+		File expectedOutputFile = new File(testcase1Path + "/expectedOutput.txt");
 		assertEquals(true, FileUtils.contentEquals(expectedOutputFile, outFile));
-
 		
 	}
 	
+	@Test
+	public void convertTest2() throws Exception {
+		/**
+		 * Test Case 2 - Convert the data file based on metadata. Two records have invalid dates. 
+		 *               Should not process the records that have invalid date and should log the rows that are not processed with proper reason.
+		 *               Should display the records that are rejected with proper reason in console.
+		 *               Write the proper records to the output file.
+		 * 
+		 * Input file: src/test/resources/converter/testcase_2/input.txt
+		 * Metadata file: src/test/resources/converter/testcase_2/metadata.txt
+		 * Expected output file: src/test/resources/converter/testcase_2/expectedOutput.txt
+		 * Output file: src/test/resources/converter/testcase_2/output.txt
+		 */
+		String testcase2Path = "src/test/resources/converter/testcase_2";
+		String metadataPath = new File(testcase2Path + "/metadata.txt").getAbsolutePath();
+		MetadataReader metadataReader = new MetadataReader();
+		List<Metadata> listMetadata = metadataReader.readMetadata(metadataPath);
+		
+		String inputFile = new File(testcase2Path + "/input.txt").getAbsolutePath();
+		File outFile = new File(testcase2Path + "/output.txt");
+		outFile.delete();
+		Converter inst = new Converter();
+		inst.convert(inputFile, listMetadata, outFile.getAbsolutePath());
+		
+		File expectedOutputFile = new File(testcase2Path + "/expectedOutput.txt");
+		assertEquals(true, FileUtils.contentEquals(expectedOutputFile, outFile));
+		
+	}
+
+	@Test
+	public void convertTest3() throws Exception {
+		/**
+		 * Test Case 3 - Convert the data file based on metadata. Two records have comma in the data. 
+		 *               Should enclose the column that have comma in a double quotes and write to output file.
+		 * 
+		 * Input file: src/test/resources/converter/testcase_3/input.txt
+		 * Metadata file: src/test/resources/converter/testcase_3/metadata.txt
+		 * Expected output file: src/test/resources/converter/testcase_3/expectedOutput.txt
+		 * Output file: src/test/resources/converter/testcase_3/output.txt
+		 */		
+		String testcase3Path = "src/test/resources/converter/testcase_3";
+		String metadataPath = new File(testcase3Path + "/metadata.txt").getAbsolutePath();
+		MetadataReader metadataReader = new MetadataReader();
+		List<Metadata> listMetadata = metadataReader.readMetadata(metadataPath);
+		
+		String inputFile = new File(testcase3Path + "/input.txt").getAbsolutePath();
+		File outFile = new File(testcase3Path + "/output.txt");
+		outFile.delete();
+		Converter inst = new Converter();
+		inst.convert(inputFile, listMetadata, outFile.getAbsolutePath());
+		
+		File expectedOutputFile = new File(testcase3Path + "/expectedOutput.txt");
+		assertEquals(true, FileUtils.contentEquals(expectedOutputFile, outFile));
+		
+	}
+	
+	@Test
+	public void convertTest4() throws Exception {
+		/**
+		 * Test Case 4 - Convert the data file based on metadata. one records have a negative number and other other record has invalid numeric no#. 
+		 *               Should not process the records that have invalid numeric number and should log the row that are not processed with proper reason.
+		 *               Should display the records that are rejected with proper reason in console.
+		 *               Write the proper records to the output file.
+		 * 
+		 * Input file: src/test/resources/converter/testcase_3/input.txt
+		 * Metadata file: src/test/resources/converter/testcase_3/metadata.txt
+		 * Expected output file: src/test/resources/converter/testcase_3/expectedOutput.txt
+		 * Output file: src/test/resources/converter/testcase_3/output.txt
+		 */		
+		String testcase4Path = "src/test/resources/converter/testcase_4";
+		String metadataPath = new File(testcase4Path + "/metadata.txt").getAbsolutePath();
+		MetadataReader metadataReader = new MetadataReader();
+		List<Metadata> listMetadata = metadataReader.readMetadata(metadataPath);
+		
+		String inputFile = new File(testcase4Path + "/input.txt").getAbsolutePath();
+		File outFile = new File(testcase4Path + "/output.txt");
+		outFile.delete();
+		Converter inst = new Converter();
+		inst.convert(inputFile, listMetadata, outFile.getAbsolutePath());
+		
+		File expectedOutputFile = new File(testcase4Path + "/expectedOutput.txt");
+		assertEquals(true, FileUtils.contentEquals(expectedOutputFile, outFile));
+		
+	}
 
 
 }
